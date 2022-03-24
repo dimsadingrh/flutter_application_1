a96c9542055ca8a4029324833ac765f9a30f0f4b
dimsadingrh <dimasadingrh@gmail.com>
2022-03-25 00:05:14 +0700

    Pertemuan 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'column_widget.dart';
+import 'ui/produk_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlue,
       ),
       title: "Aplikasi Flutter Pertama",
-      home: ColumnWidget(),
+      home: ProdukForm(),
     );
   }
 }
