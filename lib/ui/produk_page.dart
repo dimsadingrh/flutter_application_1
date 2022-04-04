6d2f3304f89f041f875a87fae5e928109944be7b
dimsadingrh <dimasadingrh@gmail.com>
2022-04-04 14:20:48 +0700

    pertemuan 4

--- lib/ui/produk_page.dart
+++ lib/ui/produk_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_application_1/ui/produk_form.dart';
+import 'package:flutter_application_1/ui/produk_detail.dart';
 import 'package:flutter/material.dart';
 
 class ProdukPage extends StatefulWidget {
@@ -30,25 +31,55 @@ class _ProdukPageState extends State<ProdukPage> {
       body: ListView(
         children: const [
           // List 1
-          Card(
-            child: ListTile(title: Text('Kulkas'), subtitle: Text('2500000')),
+          ItemProduk(
+            kodeProduk: "A001",
+            namaProduk: "Kulkas",
+            harga: 2500000,
           ),
           //List 2
-          Card(
-            child: ListTile(
-              title: Text('TV'),
-              subtitle: Text('5000000'),
-            ),
+          ItemProduk(
+            kodeProduk: "A002",
+            namaProduk: "TV",
+            harga: 5000000,
           ),
           //List 3
-          Card(
-            child: ListTile(
-              title: Text('Mesin Cuci'),
-              subtitle: Text('1500000'),
-            ),
-          )
+          ItemProduk(
+            kodeProduk: "A003",
+            namaProduk: "Mesin Cuci",
+            harga: 1500000,
+          ),
         ],
       ),
+    );
+  }
+}
+
+class ItemProduk extends StatelessWidget {
+  final String? kodeProduk;
+  final String? namaProduk;
+  final int? harga;
+  const ItemProduk({Key? key, this.kodeProduk, this.namaProduk, this.harga})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        child: ListTile(
+          title: Text(namaProduk.toString()),
+          subtitle: Text(harga.toString()),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ProdukDetail(
+                      kodeProduk: kodeProduk,
+                      namaProduk: namaProduk,
+                      harga: harga,
+                    )));
+      },
     );
   }
 }
